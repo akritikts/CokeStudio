1e4679007655aa70c33baa7e46bb893afb9237ef
akriti <akritikts@gmail.com>
2017-03-18 21:26:27 +0530

    class checkNetworkConnection added

---
+++ app/src/main/java/in/silive/cokestudio/network/CheckNetworkConnection.java
@@ -0,0 +1,30 @@
+package in.silive.cokestudio.network;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by akriti on 18/3/17.
+ */
+
+public class CheckNetworkConnection {
+    public Context context;
+
+    public CheckNetworkConnection(Context context) {
+        this.context = context;
+    }
+
+    public boolean check_connection() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = connectivityManager.getActiveNetworkInfo();
+        if (info == null) {
+            return false;
+        } else {
+            return true;
+        }
+
+
+    }
+
+}
