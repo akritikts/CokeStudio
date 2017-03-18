e1b239ff076db104adf1ad8c01fe4770becd0ab0
akriti <akritikts@gmail.com>
2017-03-18 21:45:11 +0530

    main-activity layout created

--- app/src/main/java/in/silive/cokestudio/activities/MainActivity.java
+++ app/src/main/java/in/silive/cokestudio/activities/MainActivity.java
@@ -2,14 +2,17 @@ package in.silive.cokestudio.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import in.silive.cokestudio.R;
 
 public class MainActivity extends AppCompatActivity {
+    ListView songs_list_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        songs_list_view = (ListView)findViewById(R.id.songs_list_view);
     }
 }
